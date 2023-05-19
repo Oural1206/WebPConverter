295301c40206d2020a8ed139c2532e98e05463c6
Oural <71041046+Oural1206@users.noreply.github.com>
2023-05-19 13:24:57 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import imagemin from 'imagemin'
 import imageminWebp from 'imagemin-webp'
 import { createInterface } from 'readline'
-import { unlink } from 'fs'
+import { unlink } from 'fs/promises'
 
 const readline = createInterface({ input: process.stdin, output: process.stdout })
 
@@ -10,30 +10,36 @@ function getFolderPath (imagePath) {
   return imagePath.slice(0, imagePath.length - (slitImagePath[slitImagePath.length - 1].length + 1))
 }
 
-function start () {
-  readline.question('Image path : ',  imagePath => {
-    if (imagePath[imagePath.length - 1] === ' ') {
-      imagePath = imagePath.slice(0, imagePath.length - 1)
-    }
-    const folderPath = getFolderPath(imagePath)
-    readline.question('Quality (default 50) : ', async quality => {
-      if (quality === '') {
-        quality = 50
-      } else {
-        quality = Number(quality)
-      }
-      await imagemin([imagePath], {
-        destination: folderPath,
-        plugins: [
-          imageminWebp({ quality: quality })
-        ]
-      })
-      unlink(imagePath, () => {
-        console.log('Image converted !')
-        start()
+
+async function convertToWebP(imagePath, quality = 80) {
+  try {
+    await imagemin([imagePath], {
+      destination: getFolderPath(imagePath),
+      plugins: [
+        imageminWebp({ quality: quality })
+      ]
+    })
+
+    await unlink(imagePath)
+    console.log('Original image deleted!')
+
+    console.log('Conversion to WebP completed!')
+  } catch (error) {
+    console.error('Error converting image to WebP:', error)
+  }
+}
+
+function promptUser() {
+  readline.question('Image path: ', async (imagePath) => {
+    const qualityInput = await new Promise((resolve) => {
+      readline.question('Quality (default 80): ', (answer) => {
+        resolve(answer.trim())
       })
     })
+    const quality = qualityInput ? parseInt(qualityInput) : 80
+    await convertToWebP(imagePath.slice(1, -1), quality)
+    readline.close()
   })
 }
 
-start()
+promptUser()
